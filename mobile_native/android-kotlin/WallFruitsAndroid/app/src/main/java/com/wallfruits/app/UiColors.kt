b7550313a1b9795ab83f973cef36b4970bb024d9
joelsonluiz-dot <joelson.luiz@aluno.ifsertao-pe.edu.br>
2026-05-21 12:57:27 -0300

    Sync theme tokens: Android UiColors + iOS Typography

---
+++ mobile_native/android-kotlin/WallFruitsAndroid/app/src/main/java/com/wallfruits/app/UiColors.kt
@@ -0,0 +1,15 @@
+package com.wallfruits.app
+
+import androidx.compose.ui.graphics.Color
+
+object UiColors {
+    val Primary = Color(0xFF6C63FF)
+    val PrimaryVariant = Color(0xFF8B5CF6)
+    val Secondary = Color(0xFF00D4FF)
+    val Background = Color(0xFF0B0F1A)
+    val Surface = Color(0xFF121826)
+    val OnSurface = Color(0xFFF8FAFF)
+    val Input = Color(0xFF1A2235)
+    val Success = Color(0xFF16A34A)
+    val Error = Color(0xFFDC2626)
+}
---
+++ mobile_native/ios-swift/WallFruitsiOS/Sources/Design/Typography.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+enum WFTypography {
+    static let h1 = Font.system(size: 28, weight: .bold)
+    static let h2 = Font.system(size: 22, weight: .semibold)
+    static let body = Font.system(size: 16, weight: .regular)
+    static let small = Font.system(size: 13, weight: .regular)
+}
